923c679e9e71e7e28397c2e61a2a697cd3edbae3
LetíciaCM <160792986+minucellli@users.noreply.github.com>
2025-03-25 13:47:03 -0300

    Update Ex01.py

--- Lista01.py/Ex01.py
+++ Lista01.py/Ex01.py
@@ -1,1 +1,20 @@
+#média aritmética
+nota1 = int(input("Digite a primeira nota: ") )
+nota2 = int(input("Digite a segunda nota: ") )
+nota3 = int(input("Digite a terceira nota: ") )
+media_aritmetica = (nota1 + nota2 + nota3) / 3
+print("A média aritmética das notas é:", media_aritmetica)
 
+#A média ponderada considerando os pesos 2, 2 e 3
+nota1 = int(input("Digite a primeira nota: ") )
+nota2 = int(input("Digite a segunda nota: ") )
+nota3 = int(input("Digite a terceira nota: ") )
+media_ponderada = (nota1 * 2 + nota2 * 2 + nota3 * 3) / 7
+print("A média ponderada das notas é:", media_ponderada)
+
+#A média ponderada considerando os pesos 1, 2 e 2
+nota1 = int(input("Digite a primeira nota: ") )
+nota2 = int(input("Digite a segunda nota: ") )
+nota3 = int(input("Digite a terceira nota: ") )
+media_ponderada = (nota1 * 1 + nota2 * 2 + nota3 * 2) / 5
+print("A média ponderada das notas é:", media_ponderada)
