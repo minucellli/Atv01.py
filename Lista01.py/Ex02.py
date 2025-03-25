53bce4bb5661809b57fac3119a64953f4dbc5dda
LetíciaCM <160792986+minucellli@users.noreply.github.com>
2025-03-25 13:58:07 -0300

    Update Ex02.py

--- Lista01.py/Ex02.py
+++ Lista01.py/Ex02.py
@@ -1,1 +1,12 @@
+#Solicite um valor em segundos e converta para dias, horas e minutos. 
+segundos = int(input("Digite um valor em segundos: "))
+dias = segundos // 86400
+print(f"{segundos} segundos equivale a: {dias} dias.")
+horas = segundos // 3600
+print(f"{segundos} segundos equivale a: {horas} horas.")
+minutos = segundos // 60
+print(f"{segundos} segundos equivale a: {minutos} minutos.") 
 
+# o dia equivale a = 86400 segundos
+# a hora equivale a = 3600 segundos
+# o minuto equivale a = 60 segundos
