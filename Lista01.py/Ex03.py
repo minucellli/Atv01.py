ebfd8830e5f1b1738636a87b3087f75fc1e716e5
LetíciaCM <160792986+minucellli@users.noreply.github.com>
2025-03-25 14:06:24 -0300

    Update Ex03.py

--- Lista01.py/Ex03.py
+++ Lista01.py/Ex03.py
@@ -1,1 +1,7 @@
-
+#Peça ao usuário o valor total das mercadorias compradas. Se for menor que R$500, não há imposto. Caso contrário, aplique uma taxa de 50% sobre o valor que ultrapassar os R$500.
+valor_compras = float(input("Digite o valor total das mercadorias compradas: "))
+if valor <= 500: 
+    print("Não há imposto.")
+else:
+    impostos = (valor - 500) * 0.5
+    print(f"O valor do imposto a ser pago na compra é de R$ {impostos:}")
