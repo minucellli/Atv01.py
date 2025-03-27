8f36060271985830f79a6ee73dc048f36d607766
LetíciaCM <160792986+minucellli@users.noreply.github.com>
2025-03-27 12:57:43 -0300

    Update Ex09.py

--- Lista01.py/Ex09.py
+++ Lista01.py/Ex09.py
@@ -1,1 +1,10 @@
+#Peça três números ao usuário e informe qual é o maior e qual é o menor.
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+numero3 = float(input("Digite o terceiro número: "))
 
+maior = max(numero1, numero2, numero3)
+menor = min(numero1, numero2, numero3)
+
+print(f"O maior número é: {maior}")
+print(f"O menor número é: {menor}")
