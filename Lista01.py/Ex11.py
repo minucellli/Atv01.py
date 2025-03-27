691669c49bfd436a9415eb87e1de0aa0e51beebb
LetíciaCM <160792986+minucellli@users.noreply.github.com>
2025-03-27 13:04:02 -0300

    Update Ex11.py

--- Lista01.py/Ex11.py
+++ Lista01.py/Ex11.py
@@ -1,1 +1,7 @@
-
+#Crie um sistema de login que não permite que o nome de usuário e a senha sejam iguais
+nome_user = input("Digite seu nome de usuário: ")
+senha = input("Digite a sua senha: ")
+if nome_user == senha:
+    print("Erro: O nome de usuário e a senha não podem ser iguais. Por favor, tente novamente.")
+else:
+    print("Login bem-sucedido!")
