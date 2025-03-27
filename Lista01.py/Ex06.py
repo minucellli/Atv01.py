047fa1b521dcb8e7c43359c1b0a4a89eb4553cd0
LetíciaCM <160792986+minucellli@users.noreply.github.com>
2025-03-27 12:46:40 -0300

    Update Ex06.py

--- Lista01.py/Ex06.py
+++ Lista01.py/Ex06.py
@@ -1,1 +1,19 @@
+#Crie um programa que gere e exiba os 100 primeiros números primos.
+def primo(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
+primos = []
+n = 2
+
+while len(primos) < 100:
+    if primo(n):
+        primos.append(n)
+    n += 1
+
+print("Os 100 primeiros números primos são: ")
+print(primos)
